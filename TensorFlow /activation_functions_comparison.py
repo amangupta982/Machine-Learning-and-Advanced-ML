dc35d96c1891e53ae3ba55ec2eb1194c234b3c70
Aman Gupta <amanmacair98@gmail.com>
2026-01-19 23:50:21 +0530

    Comparison of various activation functions

---
+++ TensorFlow /activation_functions_comparison.py
@@ -0,0 +1,13 @@
+import tensorflow as tf 
+
+# Input tensor
+input_tensor=tf.constant([[-2.0, -1.0, 0.0, 1.0, 2.0]])
+
+# Apply activation functions
+relu_output=tf.nn.relu(input_tensor)
+sigmoid_output=tf.nn.sigmoid(input_tensor)
+
+#Output
+print("Input:", input_tensor.numpy())
+print("ReLU Output:", relu_output.numpy())
+print("Sigmoid Output:", sigmoid_output.numpy())
