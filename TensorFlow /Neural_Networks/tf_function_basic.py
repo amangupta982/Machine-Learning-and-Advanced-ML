d453a425d65855344c3a807f62c264cae85cb303
Aman Gupta <amanmacair98@gmail.com>
2026-01-28 21:28:29 +0530

    Code for the graph wx+b

---
+++ TensorFlow /Neural_Networks/tf_function_basic.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+# A simple regular Python function
+def a_regular_function(x, y, b):
+    return tf.matmul(x, y) + b
+
+# Convert it into a TensorFlow Function (Graph mode)
+a_function_that_uses_a_graph = tf.function(a_regular_function)
+
+# Make some tensors
+x1 = tf.constant([[1.0, 2.0]])
+y1 = tf.constant([[2.0], [3.0]])
+b1 = tf.constant(4.0)
+
+# Normal Python function call
+orig_value = a_regular_function(x1, y1, b1).numpy()
+
+# TensorFlow Function call
+tf_function_value = a_function_that_uses_a_graph(x1, y1, b1).numpy()
+
+print("Original Function Output:", orig_value)
+print("TensorFlow Function Output:", tf_function_value)
